bf3402bd80908df55687c9ec251cbad705ea44fc
Mike Dodd <mdodd@natera.com>
2019-01-05 21:03:40 -0800

    Day22 done

--- src/day22.rs
+++ src/day22.rs
@@ -1,12 +1,12 @@
 use aoc_runner_derive::aoc;
-use std::mem;
-use std::collections::HashMap;
-use std::collections::hash_map::Entry;
 use std::cmp::min;
 use std::fmt;
 use std::fmt::Formatter;
 use std::fmt::Error;
 use std::fmt::Write;
+use std::cmp::max;
+use std::collections::VecDeque;
+use std::collections::HashSet;
 
 const DEPTH: usize = 11820;
 const TARGET: (usize, usize) = (7, 782);
@@ -59,48 +59,100 @@ impl fmt::Debug for Eqp {
     }
 }
 
+struct Loc {
+    cell: MapCell,
+    erosion_level: usize,
+    // For each equipment type, store a pointer back
+    // to the next step of the traceback to the origin
+    // along the shortest (currently known) path.
+    paths: [Option<(usize, (usize, usize, Eqp))>; 3],
+}
+
 struct Map {
-    map: Vec<Vec<MapCell>>,
+    map: Vec<Vec<Loc>>,
     depth: usize,
     target: (usize, usize),
+    to_check: VecDeque<(usize, usize, Eqp)>,
+    to_check_hash: HashSet<(usize, usize, Eqp)>,
+    shortest_time: Option<usize>,
+    destination: (usize, usize, Eqp),
 }
 
 impl Map {
-    fn ensure_size(&mut self, x: usize, y: usize) {
-        // Expand our map if we need to. Assume the row vectors all have the same size.
-        if y >= self.map.len() || x >= self.map[0].len() {
-            mem::replace(map, Self::new(self.depth, self.target, (x, y)));
-        }
-
-    }
 
     fn new(depth: usize, target: (usize, usize), limit: (usize, usize)) -> Self {
-        let mut geo_idx = vec![vec![0; limit.0 + 1]; limit.1 + 1];
-        for y in 0..=limit.1 {
-            for x in 0..=limit.0 {
-                let idx: usize = if (x, y) == target {
+
+        let mut ret = Self {
+            map: Vec::new(),
+            depth,
+            target,
+            to_check: VecDeque::new(),
+            to_check_hash: HashSet::new(),
+            shortest_time: None,
+            destination: (target.0, target.1, EQP_T),
+        };
+        ret.to_check.push_front((0, 0, EQP_T));
+        ret.to_check_hash.insert((0, 0, EQP_T));
+        ret.ensure_size(limit.0, limit.1);
+        ret.map[0][0].paths[EQP_T.0] = Some((0, (0, 0, EQP_T)));
+        ret
+    }
+
+    /// Ensure that we can access coordinate (x_limit, y_limit) on the map.
+    /// Expand the map if necessary to accommodate.
+    fn ensure_size(&mut self, x_limit: usize, y_limit: usize) {
+        let prev_y_size = self.map.len();
+        let prev_x_size = if prev_y_size == 0 {
+            0
+        } else {
+            self.map[0].len()
+        };
+        let new_y_size = y_limit + 1;
+        let new_x_size = x_limit + 1;
+
+        if new_y_size <= prev_y_size && new_x_size <= prev_x_size {
+            return;
+        }
+        //println!("size={}x{} expand to {}x{}", prev_x_size, prev_y_size, new_x_size, new_y_size);
+
+        // Attempt to do a single set of allocations if we are expanding the map.
+        self.map.reserve(max(new_y_size, prev_y_size) - prev_y_size);
+        for y in 0..self.map.len() {
+            self.map[y].reserve(max(new_x_size, prev_x_size) - prev_x_size);
+        }
+
+        // Fill in any missing values.
+        for y in 0..max(prev_y_size, new_y_size) {
+            let x_start: usize;
+            if y >= self.map.len() {
+                //println!("Adding row y={}", y);
+                // Need to add a new row... populate it from x=0
+                self.map.push(Vec::with_capacity(new_x_size));
+                x_start = 0;
+            } else {
+                // Already a partial row here... populate it from x=prev_x_size
+                x_start = prev_x_size;
+            }
+            // Calculate new elements.  We are adding to len so push onto the row.
+            for x in x_start..max(prev_x_size, new_x_size) {
+                //println!("Adding: ({}, {})", x, y);
+                let idx: usize = if (x, y) == self.target {
                     0
                 } else {
                     match (x, y) {
                         (0, 0) => 0,
                         (x, 0) => x * 16807,
                         (0, y) => y * 48271,
-                        (x, y) => geo_idx[y][x-1] * geo_idx[y-1][x],
+                        (x, y) => self.map[y][x - 1].erosion_level * self.map[y - 1][x].erosion_level,
                     }
                 };
-                geo_idx[y][x] = (idx + depth) % 20183;
+                let erosion_level = (idx + self.depth) % 20183;
+                self.map[y].push(Loc {
+                    cell: MapCell(erosion_level % 3),
+                    erosion_level,
+                    paths: [None; 3],
+                });
             }
-        }
-        let mut ret = vec![vec![MapCell(0); limit.0 + 1]; limit.1 + 1];
-        for y in 0..geo_idx.len() {
-            for x in 0..geo_idx[y].len() {
-                ret[y][x] = MapCell(geo_idx[y][x] % 3);
-            }
-        }
-        Self {
-            depth,
-            target,
-            map: ret,
         }
     }
 
@@ -113,7 +165,7 @@ impl Map {
                 } else if (x, y) == self.target {
                     'T'
                 } else {
-                    match self.map[y][x] {
+                    match self.map[y][x].cell {
                         REG_R => '.',
                         REG_W => '=',
                         REG_N => '|',
@@ -130,15 +182,14 @@ impl Map {
         let mut risk_level: usize = 0;
         for y in 0..=self.target.1 {
             for x in 0..=self.target.0 {
-                risk_level += self.map[y][x].0;
+                risk_level += self.map[y][x].cell.0;
             }
         }
         risk_level
     }
 
     fn valid_state(&mut self, x: usize, y: usize, e: Eqp) -> bool {
-        self.ensure_size(x, y);
-        match self.map[y][x] {
+        match self.map[y][x].cell {
             REG_R if e == EQP_E => false,
             REG_N if e == EQP_C => false,
             REG_W if e == EQP_T => false,
@@ -146,33 +197,53 @@ impl Map {
         }
     }
 
+    fn check_next(&mut self, start: &(usize, usize, Eqp), start_cost: usize, step: &(usize, usize, Eqp)) {
+        if step == start {
+            return;
+        }
+        if !self.valid_state(step.0, step.1, step.2) {
+            return;
+        }
+        let step_cost = if start.2 == step.2 {
+            1usize
+        } else {
+            7
+        };
+        let new_path_cost = start_cost + step_cost;
+
+        if let Some(t) = self.shortest_time {
+            if new_path_cost > t {
+                // Already a suboptimal path
+                return;
+            }
+        }
+
+        let existing_path = &self.map[step.1][step.0].paths[(step.2).0];
+        if existing_path.is_none() || existing_path.unwrap().0 > new_path_cost {
+            self.map[step.1][step.0].paths[(step.2).0] = Some((new_path_cost, *start));
+            if !self.to_check_hash.contains(step) {
+                self.to_check_hash.insert(*step);
+                self.to_check.push_back(*step);
+            }
+        }
+    }
+
     fn find_shortest_time(&mut self) -> usize {
         // Do a breadth-first search to find the shortest-time path from the origin to the target.
         // Note that the solution space incorporates both the x, y location as well as the equipped item.
-        let mut to_check: Vec<(usize, usize, Eqp)> = Vec::new();
-        to_check.push((0, 0, EQP_T));
-        // For locations we have already found a path to, store information about the quickest route there.
-        let mut paths: HashMap<(usize, usize, Eqp), (usize, Vec<(usize, usize, Eqp)>)> = HashMap::new();
-        paths.insert((0, 0, EQP_T), (0, vec![(0, 0, EQP_T)]));
-        // The current shortest time to target, if one has been found so far
-        let mut shortest_time: Option<usize> = None;
-        // Destination is target location and holding the torch
-        let destination: (usize, usize, Eqp) = (self.target.0, self.target.1, EQP_T);
-        while !to_check.is_empty() {
-            let start = to_check.remove(0);
-            let curr_path = match paths.entry(start) {
-                Entry::Vacant(_) => panic!("Expected path entry"),
-                Entry::Occupied(e) => e.get().clone(),
-            };
-            println!("{:?} ({} more): {:?}", start, to_check.len(), curr_path);
 
-            if start == destination {
-                shortest_time.replace(min(shortest_time.unwrap_or(curr_path.0), curr_path.0));
+        while !self.to_check.is_empty() {
+            let start = self.to_check.pop_front().unwrap();
+            self.to_check_hash.remove(&start);
+            let trace_back = &self.map[start.1][start.0].paths[(start.2).0].unwrap();
+
+            if start == self.destination {
+                self.shortest_time.replace(min(self.shortest_time.unwrap_or(trace_back.0), trace_back.0));
                 continue;
             }
 
-            if let Some(t) = shortest_time {
-                if t < curr_path.0 {
+            if let Some(t) = self.shortest_time {
+                if t < trace_back.0 {
                     // No need to check anything else. This path is already sub-optimal.
                     continue;
                 }
@@ -183,45 +254,42 @@ impl Map {
             // 2) Move if possible: up, left, right, down.
             // Each check_next call potentially adds a new item to the to_check list, updates the entry
             // in paths, and expands the map if required.
+            self.ensure_size(start.0 + 1, start.1 + 1);
             for e in (0..=2).map(|i| Eqp(i)) {
-                check_next(&mut map, &mut to_check, &mut paths, &curr_path, &(start.0, start.1, e), &shortest_time);
+                self.check_next(&start, trace_back.0, &(start.0, start.1, e));
             }
             if start.1 > 0 {
-                check_next(&mut map, &mut to_check, &mut paths, &curr_path, &(start.0, start.1 - 1, start.2), &shortest_time);
+                self.check_next(&start, trace_back.0, &(start.0, start.1 - 1, start.2));
             }
             if start.0 > 0 {
-                check_next(&mut map, &mut to_check, &mut paths, &curr_path, &(start.0 - 1, start.1, start.2), &shortest_time);
+                self.check_next(&start, trace_back.0, &(start.0 - 1, start.1, start.2));
             }
-            check_next(&mut map, &mut to_check, &mut paths, &curr_path, &(start.0 + 1, start.1, start.2), &shortest_time);
-            check_next(&mut map, &mut to_check, &mut paths, &curr_path, &(start.0, start.1 + 1, start.2), &shortest_time);
+            self.check_next(&start,trace_back.0, &(start.0 + 1, start.1, start.2));
+            self.check_next(&start,trace_back.0, &(start.0, start.1 + 1, start.2));
         }
 
-        println!("{}", self.get_map_str());
-        match paths.entry(destination) {
-            Entry::Vacant(_) => panic!("No path to destination"),
-            Entry::Occupied(o) => {
-                let path = &o.get().1;
-                let mut cost = 0;
-                let mut prev = (0, 0, EQP_T);
-                for i in 0..path.len() {
-                    let (x, y, e) = path[i];
-                    if prev.2 != e {
-                        assert_eq!((x, y), (prev.0, prev.1));
-                        cost += 7;
-                    } else if prev.0 != x {
-                        assert_eq!((y, e), (prev.1, prev.2));
-                        cost += 1;
-                    } else if prev.1 != y {
-                        assert_eq!((x, e), (prev.0, prev.2));
-                        cost += 1;
-                    }
-
-                    println!("({}, {}) {} {} -> {}", x, y, e, self.map[y][x], cost);
-                    prev = path[i];
-                }
+        //println!("{}", self.get_map_str());
+        let best_path = &self.map[self.destination.1][self.destination.0].paths[(self.destination.2).0].unwrap();
+        let mut cost = 0;
+        let mut prev = self.destination;
+        let mut curr = best_path.1;
+        while prev != (0, 0, EQP_T) {
+            let (x, y, e) = curr;
+            if prev.2 != e {
+                assert_eq!((x, y), (prev.0, prev.1));
+                cost += 7;
+            } else if prev.0 != x {
+                assert_eq!((y, e), (prev.1, prev.2));
+                cost += 1;
+            } else if prev.1 != y {
+                assert_eq!((x, e), (prev.0, prev.2));
+                cost += 1;
             }
+            prev = curr;
+            curr = self.map[curr.1][curr.0].paths[(curr.2).0].unwrap().1;
         }
-        shortest_time.unwrap()
+        assert_eq!(cost, best_path.0);
+        self.shortest_time.unwrap()
     }
 }
 
@@ -231,69 +299,10 @@ fn solve_part1(_input: &str) -> usize {
     map.get_risk_level()
 }
 
-/// For the given destination `step`, see if we need to (re-)examine paths through `step`.
-/// If so, update `to_check` for future iterations of the main loop. 
-fn check_next(map: &mut Map, to_check: &mut Vec<(usize, usize, Eqp)>, paths: &mut HashMap<(usize, usize, Eqp), (usize, Vec<(usize, usize, Eqp)>)>, curr_path: &(usize, Vec<(usize, usize, Eqp)>), step: &(usize, usize, Eqp), shortest_time: &Option<usize>) {
-    map.ensure_size(step.0, step.1);
-    print!("Checking: {:?} for {:?} ", step, map.map[step.1][step.0]);
-    if map.valid_state(step.0, step.1, step.2) {
-        println!("... valid");
-    } else {
-        println!("... invalid");
-        return;
-    }
-    let prev = curr_path.1.last().unwrap();
-    if step == prev {
-        return;
-    }
-    let step_cost = if prev.2 == step.2 {
-        1
-    } else {
-        7
-    };
-    let new_path_cost = curr_path.0 + step_cost;
-
-    if let Some(t) = *shortest_time {
-        if new_path_cost > t {
-            // Already a suboptimal path
-            return;
-        }
-    }
-
-    let mut new_path = curr_path.1.to_owned();
-    new_path.push(*step);
-
-    match paths.entry(*step) {
-        Entry::Vacant(v) => {
-            println!("Adding new: {:?}", step);
-            v.insert((new_path_cost, new_path));
-            if !to_check.contains(step) {
-                to_check.push(*step);
-            } else {
-                println!("...already in list");
-            }
-        }
-        Entry::Occupied(ref mut o) => {
-            let (old_path_cost, old_path) = o.get_mut();
-            if new_path_cost < *old_path_cost {
-                println!("Found better path {} -> {} {:?}", old_path_cost, new_path_cost, step);
-                *old_path_cost = new_path_cost;
-                *old_path = new_path;
-                if !to_check.contains(step) {
-                    to_check.push(*step);
-                } else {
-                    println!("...already in list");
-                }
-            }
-        }
-    }
-    println!("{:?}", paths.entry(*step));
-}
-
 #[aoc(day22, part2)]
 fn solve_part2(_input: &str) -> usize {
-    let mut map = get_map(DEPTH, TARGET, TARGET);
-    find_shortest_time(&mut map, TARGET)
+    let mut map = Map::new(DEPTH, TARGET, TARGET);
+    map.find_shortest_time()
 }
 
 #[cfg(test)]
@@ -357,11 +366,11 @@ M=.|=.|.|=.|=|=.
                 EQP_T => 'T',
                 _ => panic!("Unknown equipment {}", t.2),
             };
-            let reg = match map.map[t.1][t.0] {
+            let reg = match map.map[t.1][t.0].cell {
                 REG_R => '.',
                 REG_N => '|',
                 REG_W => '=',
-                _ => panic!("Unknown map cell {}", map.map[t.1][t.0]),
+                _ => panic!("Unknown map cell {}", map.map[t.1][t.0].cell),
             };
 
             assert_eq!(t.3, map.valid_state(t.0, t.1, t.2), "({}, {}) {} + {}: expected {}", t.0, t.1, reg, eqp, t.3);
